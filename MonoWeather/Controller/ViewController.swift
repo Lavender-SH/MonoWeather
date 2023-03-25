75347c950534b790f56bc292783be8f4d2379ce8
Lavender <susie204@naver.com>
2023-03-25 12:09:19 +0900

    completion 하기전

--- MonoWeather/Controller/ViewController.swift
+++ MonoWeather/Controller/ViewController.swift
@@ -6,14 +6,52 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate{
+    
 
+    @IBOutlet weak var ondoLB: UILabel!
+    
+    var weatherManager = WeatherManager()
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setting()
+        locationManager.delegate = self
+        weatherManager.delegate = self
+        searchTextfield.delegate = self
+        
+        
+        locationManager.requestLocation()
+        locationManager.requestWhenInUseAuthorization()
     }
 
-
+    
+    
+    
+    
+    func setting(){
+        
+        let tabBarVC = UITabBarController()
+        
+        let vc1 = UINavigationController(rootViewController: SettingViewController())
+        let vc2 = FirstViewController()
+        let vc3 = SecondViewController()
+        
+        vc1.title = "메인화면"
+        vc2.title = "디테일화면"
+        
+        tabBarVC.setViewControllers([vc1,vc2], animated:false)
+        tabBarVC.modalPresentationStyle = .fullScreen
+        tabBarVC.tabBar.backgroundColor = .white
+        
+        guard let items = tabBarVC.tabBar.items else {return}
+        items[0].image = UIImage(systemName: "cloud.rain")
+        items[1].image = UIImage(systemName: "cloud.rain.circle")
+        
+    //present(tabBarVC,animated: true, completion: nil)
+    }
 }
 
---
+++ MonoWeather/Networking/NetworkManager.swift
@@ -0,0 +1,73 @@
+//
+//  File.swift
+//  MonoWeather
+//
+//  Created by 이승현 on 2023/03/25.
+//
+
+import Foundation
+import CoreLocation
+
+protocol WeatherManagerDelegate {
+    func didUpdateWeather(_weatherManager: WeatherManager, weather: WeatherModel)
+    func didFailWithError(error: Error)
+}
+
+struct WeatherManager{
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=82ba15ba68cefeff502358350f966d4d&units=metric"
+    var delegate: WeatherManagerDelegate?
+    
+    func fetchWeather(latitude: CLLocationDegrees, longitude: CLLocationDegrees){
+        let urlString = "\(weatherURL)&lat=\(latitude)&lon=\(longitude)"
+        performRequest(with: urlString)
+    }
+    
+    func fetchWeather(cityName: String){
+        let urlString = "\(weatherURL)&q=\(cityName)"
+        performRequest(with: urlString)
+    }
+    func performRequest(with urlString: String){
+        
+        //1. URL 인스턴스 생성
+        if let url = URL(string: urlString){
+            //2. URLSession 인스턴스 생성
+            let session = Foundation.URLSession(configuration: .default)
+            
+            //3. URLSession 에게 할 일 주기
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil{
+                    delegate?.didFailWithError(error: error!)
+                    return
+                }
+                
+                //Data 타입으로 데이터를 받아옴..일반 string, int 가 아님
+                if let safeData = data {
+                    if let weather = self.parseJSON(weatherData: safeData){
+                        delegate?.didUpdateWeather(_weatherManager: self, weather: weather)
+                    }
+                }
+            }
+            
+            //4. 할 일 시작
+            task.resume()
+        }
+    }
+    func parseJSON(weatherData: Data)->WeatherModel? {
+        
+        let decoder = JSONDecoder() //an object that can decode JSON Objects
+        
+        do{
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            let id = decodedData.weather[0].id
+            let temp = decodedData.main.temp
+            let name = decodedData.name
+            
+            let weather = WeatherModel(conditionID: id, cityName: name, temperature: temp)
+            return weather
+            
+        }catch{
+            delegate?.didFailWithError(error: error)
+            return nil
+        }
+    }
+}
