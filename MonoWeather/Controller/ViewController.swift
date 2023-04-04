e20cab2513427269d72868b65218c08f63bcf68a
Lavender <susie204@naver.com>
2023-04-05 00:38:20 +0900

    textField 구현

--- MonoWeather/Controller/ViewController.swift
+++ MonoWeather/Controller/ViewController.swift
@@ -10,79 +10,115 @@ import CoreLocation
 
 class ViewController: UIViewController {
     
-
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var conditionImageview: UIImageView!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
     
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+    }
+    
+    @IBAction func findCurrentLocationButton(_ sender: UIButton) { locationManager.requestLocation()
+    }
     
     var weatherManager = WeatherManager.shared
     let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setting()
+        
+        
         locationManager.delegate = self
         weatherManager.delegate = self
-        //searchTextfield.delegate = self
+        searchTextField.delegate = self
         
         locationManager.requestLocation()
         locationManager.requestWhenInUseAuthorization()
     }
     
     
-    func setting(){
-        
-        let tabBarVC = UITabBarController()
-        
-        let vc1 = UINavigationController(rootViewController: SettingViewController())
-        let vc2 = FirstViewController()
-        let vc3 = SecondViewController()
-        
-        vc1.title = "메인화면"
-        vc2.title = "디테일화면"
-        
-        tabBarVC.setViewControllers([vc1,vc2], animated:false)
-        tabBarVC.modalPresentationStyle = .fullScreen
-        tabBarVC.tabBar.backgroundColor = .white
-        
-        guard let items = tabBarVC.tabBar.items else {return}
-        items[0].image = UIImage(systemName: "cloud.rain")
-        items[1].image = UIImage(systemName: "cloud.rain.circle")
-        
-    //present(tabBarVC,animated: true, completion: nil)
-    }
+//    func setting(){
+//
+//        let tabBarVC = UITabBarController()
+//
+//        let vc1 = UINavigationController(rootViewController: SettingViewController())
+//        let vc2 = FirstViewController()
+//        let vc3 = SecondViewController()
+//
+//        vc1.title = "메인화면"
+//        vc2.title = "디테일화면"
+//
+//        tabBarVC.setViewControllers([vc1,vc2], animated:false)
+//        tabBarVC.modalPresentationStyle = .fullScreen
+//        tabBarVC.tabBar.backgroundColor = .white
+//
+//        guard let items = tabBarVC.tabBar.items else {return}
+//        items[0].image = UIImage(systemName: "cloud.rain")
+//        items[1].image = UIImage(systemName: "cloud.rain.circle")
+//
+//        //present(tabBarVC,animated: true, completion: nil)
+//    }
 }
-
-
-
-// MARK: - 확장 WeatherManagerDelegate
-extension ViewController: WeatherManagerDelegate {
-    func didUpdateWeather(_weatherManager: WeatherManager, weather: WeatherModel) {
-        DispatchQueue.main.async {
-            self.temperatureLabel.text = weather.temperatureString
-            self.conditionImageview.image = UIImage(systemName: weather.conditionName)
-            self.cityLabel.text = weather.cityName
+    
+    
+    
+    // MARK: - 확장 WeatherManagerDelegate
+    extension ViewController: WeatherManagerDelegate {
+        func didUpdateWeather(_weatherManager: WeatherManager, weather: WeatherModel) {
+            DispatchQueue.main.async {
+                self.temperatureLabel.text = weather.temperatureString
+                self.conditionImageview.image = UIImage(systemName: weather.conditionName)
+                self.cityLabel.text = weather.cityName
+            }
+        }
+        func didFailWithError(error: Error) {
+            print(error)
         }
     }
-    func didFailWithError(error: Error) {
-        print(error)
+    
+    
+    // MARK: - 확장 CLLocationManagerDelegate
+    extension ViewController: CLLocationManagerDelegate {
+        func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+            if let location = locations.last{
+                locationManager.stopUpdatingLocation()
+                let lat = location.coordinate.latitude
+                let lon = location.coordinate.longitude
+                weatherManager.fetchWeather(latitude: lat, longitude: lon)
+            }
+        }
+        func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+            print(error)
+        }
+        
     }
-}
-
-
-// MARK: - 확장 CLLocationManagerDelegate
-extension ViewController: CLLocationManagerDelegate {
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        if let location = locations.last{
-            locationManager.stopUpdatingLocation()
-            let lat = location.coordinate.latitude
-            let lon = location.coordinate.longitude
-            weatherManager.fetchWeather(latitude: lat, longitude: lon)
+    
+    
+    
+    extension ViewController: UITextFieldDelegate {
+        func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+            searchTextField.endEditing(true)
+            return true
+        }
+        
+        func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+            
+            if textField.text != ""{
+                return true
+            }
+            else{
+                textField.placeholder =  "Type a city"
+                return false
+            }
+        }
+        
+        func textFieldDidEndEditing(_ textField: UITextField) {
+            if let city = searchTextField.text{
+                weatherManager.fetchWeather(cityName: city)
+            }
+            searchTextField.text = ""
         }
     }
-    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        print(error)
-    }
 
-}
+
